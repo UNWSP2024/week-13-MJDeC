7d923fbe233df23f44a8e2d352f3b2350e419aaa
MJDeC <mjdecaro@students.unwsp.edu>
2024-12-03 20:09:35 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -24,30 +24,31 @@ def add_cities_table(cursor):
     cursor.execute('''CREATE TABLE Cities (CityID INTEGER PRIMARY KEY NOT NULL,CityName TEXT,Population REAL)''')
 
 #add 20 rows to cities table
-def add_cities(cursor):
     cities_pop = [(1,'Tokyo',38001000),
-                  (2,'Delhi',25703168),
-                  (3,'Shanghai',23740778),
-                  (4,'Sao Paulo',21066245),
-                  (5,'Mumbai',21042538),
-                  (6,'Mexico City',20998543),
-                  (7,'Beijing',20383994),
-                  (8,'Osaka',20237645),
-                  (9,'Cairo',18771769),
-                  (10,'New York',18593220),
-                  (11,'Dhaka',17598228),
-                  (12,'Karachi',16617644),
-                  (13,'Buenos Aires',15180176),
-                  (14,'Kolkata',14864919),
-                  (15,'Istanbul',14163989),
-                  (16,'Chongqing',13331579),
-                  (17,'Lagos',13122829),
-                  (18,'Manila',12946263),
-                  (19,'Rio de Janeiro',12902306),
-                  (20,'Guangzhou',12458130)]
+                (2,'Delhi',25703168),
+                (3,'Shanghai',23740778),
+                (4,'Sao Paulo',21066245),
+                (5,'Mumbai',21042538),
+                (6,'Mexico City',20998543),
+                (7,'Beijing',20383994),
+                (8,'Osaka',20237645),
+                (9,'Cairo',18771769),
+                (10,'New York',18593220),
+                (11,'Dhaka',17598228),
+                (12,'Karachi',16617644),
+                (13,'Buenos Aires',15180176),
+                (14,'Kolkata',14864919),
+                (15,'Istanbul',14163989),
+                (16,'Chongqing',13331579),
+                (17,'Lagos',13122829),
+                (18,'Manila',12946263),
+                (19,'Rio de Janeiro',12902306),
+                (20,'Guangzhou',12458130)]
+    cursor.executemany("insert into cities values (?,?,?)", cities_pop)
     #print contents of cities table
-    for row in cities_pop:
-      print(row)
+    for row in cursor.execute("select * from cities"):
+        print(row)
+      
   
 
 #execute main
