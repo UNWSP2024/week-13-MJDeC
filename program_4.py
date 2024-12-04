7b6eefa6c1ac877e8227917dc031f47f845644b0
MJDeC <mjdecaro@students.unwsp.edu>
2024-12-04 14:58:38 -0600

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -1,5 +1,19 @@
+import sqlite3
 
-def add_phonebook_table(cursor):
+#define main function
+def main():
+    #connect to database
+    connection=sqlite3.connect('entries.db')
+    #get cursor
+    cursor=connection.cursor()
+    #add entries table
+    entries_table(cursor)
+    #commit changes and close connection
+    connection.commit()
+    connection.close()
+
+
+def entries_table(cursor):
     #if table exists drop table
     cursor.execute('DROP TABLE IF EXISTS Entries')
 
@@ -7,5 +21,10 @@ def add_phonebook_table(cursor):
     cursor.execute('''CREATE TABLE Entries (CityID INTEGER PRIMARY KEY NOT NULL,Name TEXT,Number Integer)''')
 
 #add rows to phonebook table
-    phonebook_pop = [(1,'Micah DeCaro',6514300544)]
-    cursor.executemany("insert into phonebook values (?,?,?)", entries)
+    entries_pop = [(1,'Micah DeCaro',6514300544)]
+    cursor.executemany("insert into entries values (?,?,?)", entries)
+    for row in cursor.execute("select * from entries"):
+        print(row)
+
+if __name__ == '__main__':
+    main()
