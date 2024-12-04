57a6abb4e888c7c97681eb751b3ad876807d5e7b
MJDeC <mjdecaro@students.unwsp.edu>
2024-12-04 14:40:25 -0600

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -1,10 +1,10 @@
 
-def add_cities_table(cursor):
+def add_phonebook_table(cursor):
     #if table exists drop table
-    cursor.execute('DROP TABLE IF EXISTS Cities')
+    cursor.execute('DROP TABLE IF EXISTS Entries')
 
     #create table
-    cursor.execute('''CREATE TABLE Entries (CityID INTEGER PRIMARY KEY NOT NULL,CityName TEXT,Population REAL)''')
+    cursor.execute('''CREATE TABLE Entries (CityID INTEGER PRIMARY KEY NOT NULL,Name TEXT,Number Integer)''')
 
 #add rows to phonebook table
     phonebook_pop = [(1,'Micah DeCaro',6514300544)]
