bfaff8791c970327e840f923ad11e9e7d55206c3
MJDeC <mjdecaro@students.unwsp.edu>
2024-12-04 20:52:57 -0600

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -18,13 +18,19 @@ def entries_table(cursor):
     cursor.execute('DROP TABLE IF EXISTS Entries')
 
     #create table
-    cursor.execute('''CREATE TABLE Entries (CityID INTEGER PRIMARY KEY NOT NULL,Name TEXT,Number Integer)''')
+    cursor.execute('''CREATE TABLE Entries (EntriesID INTEGER PRIMARY KEY NOT NULL,Name TEXT,Number Integer)''')
 
 #add rows to phonebook table
-    entries_pop = [(1,'Micah DeCaro',6514300544)]
-    cursor.executemany("insert into entries values (?,?,?)", entries)
-    for row in cursor.execute("select * from entries"):
-        print(row)
+    entries_pop = [(1,'Micah DeCaro',6514300544),
+                  (2,'Bob Bobbson',123456),
+                  (3,'Brandon Sanderson',99999999),
+                  (4,'Steve Jobs',234566)]
+    cursor.executemany("insert into entries values (?,?,?)", entries_pop)
+    #for row in cursor.execute("select * from entries"):
+        #print(row)
+    cursor.execute("select * from entries where name=:n",{"n":"Micah DeCaro"})
+    name_search=cursor.fetchall()
+    print(name_search)
 
 if __name__ == '__main__':
     main()
